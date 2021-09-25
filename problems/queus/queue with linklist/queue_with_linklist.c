e0088c02e6838c1334d72c3dad5bb22dc21fd3b5
sandeep <sandeepchauhan9630228313@gmail.com>
2021-09-25 18:45:35 +0530

    queue with linkedin

---
+++ problems/queus/queue with linklist/queue_with_linklist.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node * next;
+};
+int main()
+ {
+
+     struct node * f =NULL;
+     struct node * r =NULL;
+
+    
+     
+     return 0;
+ }
