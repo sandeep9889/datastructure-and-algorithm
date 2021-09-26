b623e529f6cbedc6881c958c6b3376704fdc6af5
sandeep <sandeepchauhan9630228313@gmail.com>
2021-09-26 15:01:24 +0530

    add of two no

---
+++ practice set/basic problems/addition_of_no.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{ 
+     int a,b;
+    printf("enter two number");
+    scanf("%d%d",&a,&b);
+    int c= a+b;
+    printf("output is %d",c);
+}
---
+++ practice set/basic problems/addition_of_no.py
@@ -0,0 +1,8 @@
+print("enter two no")
+b= int (input())
+a= int (input())
+print("your input is ",a)
+print("your input is ",b)
+
+c = a+b
+print(c)
