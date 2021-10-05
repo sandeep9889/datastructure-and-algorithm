2ccee9ec2bee3ccbd8f8ad7adb2e5f4960327da3
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-05 17:01:23 +0530

    linked_list

---
+++ problems/linkedlist/linkedlist_python/linkedlist.py
@@ -0,0 +1,19 @@
+from _typeshed import Self
+
+
+class node:
+    def __init__(self,data):
+        self.data = data
+        self.next  = None
+
+
+class linkedlist:
+    def __init__(self,):
+
+        pass
+
+if __name__ =="__main__":
+    first_node = node(56)
+    second_node = node(64)
+    Self
+
