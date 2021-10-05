6c36d56b3d997833bd616aeeca466b83123696c3
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-05 21:48:02 +0530

    linked_list

--- problems/linkedlist/linkedlist_python/linkedlist.py
+++ problems/linkedlist/linkedlist_python/linkedlist.py
@@ -1,17 +1,20 @@
-from _typeshed import Self
+
 class node:
     def __init__(self,data):
         self.data = data
         self.next  = None
 
-
 class linkedlist:
     def __init__(self,):
 
-        pass
+        self.head =None
 
 if __name__ =="__main__":
-    first_node = node(56)
-    second_node = node(64)
-    Self
-
+    llist = linkedlist()
+    llist.head = node(56)
+    second = node(65)
+    third = node(63)
+    
+    llist.head.next = second
+    second.next =third
+    print(linkedlist(llist))
