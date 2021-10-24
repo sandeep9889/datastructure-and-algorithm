5a67964e1f0b99d1e215203fb8457dbf0bd2fb8b
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-24 16:15:20 +0530

    bt tree

--- problems/sorting/bubblesort.c
+++ problems/sorting/bubblesort.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
  int i;
-printarr(int A, int n)
+printarr(A,n)
  {
+
   for (i=0;i<n;i++)
   { 
-      printf("%d",A[i]);
+    printf("%d", A[i]);
   } 
  }
 
 int main()
 { 
-     int A[] = {2,56,65,23,54,84,25};
+    int A[] = {2,56,65,23,54,84,25};
+    
     int n = 7;
     printarr(A,n);
     return 0;
